5887ac3700b7124cc55b9ad96de9e1d16ebae62f
Suryanandan Babbar <suryanandanbabbar@gmail.com>
2025-06-25 15:06:24 +0530

    930: Binary Subarrays with sum

---
+++ 2025/binarySubarrays.java
@@ -0,0 +1,25 @@
+// LeetCode Problem: 930. Binary Subarrays With Sum
+// Link: https://leetcode.com/problems/binary-subarrays-with-sum/
+import java.util.HashMap;
+
+class binarySubarrays {
+    public int numSubarraysWithSum(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int count = 0;
+        int sum = 0;
+
+        map.put(0, 1); 
+
+        for (int num : nums) {
+            sum += num;
+
+            if (map.containsKey(sum - k)) {
+                count += map.get(sum - k);
+            }
+
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
+        }
+
+        return count;
+    }
+}
