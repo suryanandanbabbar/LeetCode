07477ff4283b348adaba3510c194a576cbf92b66
Suryanandan Babbar <suryanandanbabbar@gmail.com>
2025-06-25 10:06:14 +0530

    1207: Unique Number of Occurrences

---
+++ 2025/UniqNoOccurr.java
@@ -0,0 +1,23 @@
+// 1207. Unique Number of Occurrences
+// Link: https://leetcode.com/problems/unique-number-of-occurrences/
+
+import java.util.*;
+
+class Solution {
+    public boolean uniqueOccurrences(int[] arr) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int num : arr) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        HashSet<Integer> frequencies = new HashSet<>();
+        for (int freq : map.values()) {
+            if (!frequencies.add(freq)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
