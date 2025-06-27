60586c8af2e2c1c29665fce0a5456c72235a9e64
Suryanandan Babbar <suryanandanbabbar@gmail.com>
2025-06-27 15:14:11 +0530

    Added another approach using sorting

--- 2025/longestCommonPrefix.java
+++ 2025/longestCommonPrefix.java
@@ -3,15 +3,31 @@
 
 class Solution {
     public String longestCommonPrefix(String[] strs) {
-        if(strs.length == 0) return "";
+        // if(strs.length == 0) return "";
 
-        String prefix = strs[0];
-        for(int i = 1; i < strs.length; i++) {
-            while(strs[i].indexOf(prefix) != 0) {
-                prefix = prefix.substring(0,prefix.length() - 1);
-                if(prefix.isEmpty()) return "";
+        // String prefix = strs[0];
+        // for(int i = 1; i < strs.length; i++) {
+        //     while(strs[i].indexOf(prefix) != 0) {
+        //         prefix = prefix.substring(0,prefix.length() - 1);
+        //         if(prefix.isEmpty()) return "";
+        //     }
+        // }
+        // return prefix;
+
+        // Using Sort
+        if (strs.length == 0) return "";
+        Arrays.sort(strs);
+
+        String first = strs[0];
+        String last = strs[strs.length - 1];
+        String result = "";
+        for(int i = 0; i < first.length(); i++) {
+            if (first.charAt(i) != last.charAt(i)) {
+                break;
             }
+            result += first.charAt(i);
         }
-        return prefix;
+
+        return result;
     }
 }
