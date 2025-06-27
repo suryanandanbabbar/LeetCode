764596f76d5a9c59512aaf02e76288b28983aa04
Suryanandan Babbar <suryanandanbabbar@gmail.com>
2025-06-27 14:05:23 +0530

    Modifiying code.

--- 2025/sortByFrequency.java
+++ 2025/sortByFrequency.java
@@ -1,26 +1,27 @@
 // Sorting characters by frequency
 // Code360: https://www.naukri.com/code360/problems/sorting-characters-by-frequency_1263699
 // LeetCode: https://leetcode.com/problems/sort-characters-by-frequency/description/
+import java.util.*;
 
 class Solution {
     public static String sortByFrequency(int n, String s) {
-        // Counting the frequency of each character
-        int[] freq = new int[256]; // Assuming ASCII characters
-        for (char c : s.toCharArray()) {
-            freq[c]++;
-        }
+        // // Counting the frequency of each character
+        // int[] freq = new int[256]; // Assuming ASCII characters
+        // for (char c : s.toCharArray()) {
+        //     freq[c]++;
+        // }
 
-        // Creating a list of characters sorted by frequency
-        StringBuilder result = new StringBuilder();
-        for (int i = 0; i < 256; i++) {
-            if (freq[i] > 0) {
-                for (int j = 0; j < freq[i]; j++) {
-                    result.append((char) i);
-                }
-            }
-        }
+        // // Creating a list of characters sorted by frequency
+        // StringBuilder result = new StringBuilder();
+        // for (int i = 0; i < 256; i++) {
+        //     if (freq[i] > 0) {
+        //         for (int j = 0; j < freq[i]; j++) {
+        //             result.append((char) i);
+        //         }
+        //     }
+        // }
 
-        return result.toString();
+        // return result.toString();
 
         // LeetCode Solution:
         int[] freq = new int[128];
