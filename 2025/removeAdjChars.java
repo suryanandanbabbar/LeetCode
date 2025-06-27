0a01614dde15de746b2328c15f717f29327d0ab7
Suryanandan Babbar <suryanandanbabbar@gmail.com>
2025-06-27 10:41:24 +0530

    1047: Remove All Adjacent Duplicates in String by Stack method

--- 2025/removeAdjChars.java
+++ 2025/removeAdjChars.java
@@ -1,17 +1,36 @@
 // LeetCode Problem: 1047: Remove All Adjacent Duplicates In String
 // Link: https://leetcode.com/problems/remove-all-adjacent-duplicates-in-string/
 
+import java.util.Stack;
 class Solution {
     public String removeDuplicates(String s) {
-        StringBuilder sb = new StringBuilder();
+        // StringBuilder sb = new StringBuilder();
+
+        // for(char c: s.toCharArray()) {
+        //     if(sb.length() > 0 && sb.charAt(sb.length() - 1) == c) {
+        //         sb.deleteCharAt(sb.length() - 1); // Remove last character if it matches current
+        //     } else {
+        //         sb.append(c); // Otherwise, add current character
+        //     }
+        // }
+        // return sb.toString(); // Return the final string without adjacent duplicates
+
+        // Using Stack
+        Stack<Character> st = new Stack<>();
 
         for(char c: s.toCharArray()) {
-            if(sb.length() > 0 && sb.charAt(sb.length() - 1) == c) {
-                sb.deleteCharAt(sb.length() - 1); // Remove last character if it matches current
+            if(!st.isEmpty() && st.peek() == c) {
+                st.pop();
             } else {
-                sb.append(c); // Otherwise, add current character
+                st.push(c);
             }
         }
-        return sb.toString(); // Return the final string without adjacent duplicates
+
+        StringBuilder sb = new StringBuilder();
+        while(!st.isEmpty()) {
+            sb.append(st.pop());
+        }
+
+        return sb.toString();
     }
 }
