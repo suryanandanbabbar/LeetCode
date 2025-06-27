21fe8c8c36d7a6ff2f701f8a4f17a1d26b2419d3
Suryanandan Babbar <suryanandanbabbar@gmail.com>
2025-06-27 10:14:34 +0530

    2014: Longest Substring with k Repeating Characters

---
+++ 2025/longestSubsKTimes.java
@@ -0,0 +1,34 @@
+// LeetCode Problem : 2014. Longest Substring With K Repeating Characters
+// Link: https://leetcode.com/problems/longest-substring-with-k-repeating-characters
+import java.util.LinkedList;
+import java.util.Queue;
+
+class Solution {
+    public String longestSubsequenceRepeatedK(String s, int k) {
+        String r="";
+        Queue<String> q=new LinkedList<>();
+        for(q.add("");!q.isEmpty();) {
+            String c=q.poll();
+            for(char ch='a';ch<='z';ch++) {
+                String n=c+ch;
+                if(isK(n,s,k)) {
+                    r=n;
+                    q.add(n);
+                }
+            }
+        }
+        return r;
+    }
+    boolean isK(String s,String t,int k) {
+        int c=0,i=0;
+        for(char ch:t.toCharArray()) {
+            if(ch==s.charAt(i)) {
+                if(++i==s.length()) {
+                    i=0;
+                    if(++c==k) return true;
+                }
+            }
+        }
+        return false;
+    }
+}
