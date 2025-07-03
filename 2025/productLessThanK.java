f5555d48d812132f6274d361dbc0cb5be61535c9
Suryanandan Babbar <suryanandanbabbar@gmail.com>
2025-07-03 15:05:55 +0530

    713: Subarray Product Less Than K

---
+++ 2025/productLessThanK.java
@@ -0,0 +1,23 @@
+// LeetCode Problem: 713. Subarray Product Less Than K
+// Link: https://leetcode.com/problems/subarray-product-less-than-k/
+class productLessThanK {
+    public int numSubarrayProductLessThanK(int[] nums, int k) {
+        if (k <= 1) return 0; 
+
+        int left = 0, right = 0, product = 1, count = 0;
+
+        while (right < nums.length) {
+            product *= nums[right];
+
+            while (product >= k && left <= right) {
+                product /= nums[left];
+                left++;
+            }
+
+            count += right - left + 1; 
+            right++;
+        }
+
+        return count;
+    }
+}
