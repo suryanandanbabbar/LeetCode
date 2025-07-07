b1b0afd012c932a29b9511407813390424ff72f4
Suryanandan Babbar <suryanandanbabbar@gmail.com>
2025-07-07 10:38:41 +0530

    3174: Clear Digits

---
+++ 2025/clearDigits.java
@@ -0,0 +1,29 @@
+// LeetCode Problem: 3174. Clear Digits
+// Link: https://leetcode.com/problems/clear-digits/
+
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+
+import java.util.ArrayDeque;
+
+class Solution {
+    public String clearDigits(String s) {
+        ArrayDeque<Character> stack = new ArrayDeque<>();
+
+        for(char c: s.toCharArray()) {
+            if(Character.isDigit(c)) {
+                if(!stack.isEmpty()) 
+                    stack.pop();
+            } else {
+                stack.push(c);
+            }
+        }
+
+        StringBuilder result = new StringBuilder();
+        while(!stack.isEmpty()) {
+            result.append(stack.pop());
+        }
+
+        return result.reverse().toString();
+    }
+}
