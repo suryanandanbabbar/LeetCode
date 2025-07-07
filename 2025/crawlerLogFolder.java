aa24bf6385082bcebbcfcdf451864d82dc46041f
Suryanandan Babbar <suryanandanbabbar@gmail.com>
2025-07-07 11:54:51 +0530

    1598: Crawler Log Folder

---
+++ 2025/crawlerLogFolder.java
@@ -0,0 +1,43 @@
+// LeetCode Problem: 1598. Crawler Log Folder
+// Link: https://leetcode.com/problems/crawler-log-folder/
+
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+
+import java.util.ArrayDeque;
+
+public class crawlerLogFolder {
+    // Brute Force Approach
+    public int minOperations(String[] logs) {
+        int depth = 0;
+
+        for (String log : logs) {
+            if (log.equals("../")) {
+                if (depth > 0) {
+                    depth--;
+                }
+            } else if (!log.equals("./")) {
+                depth++;
+            }
+        }
+
+        return depth;
+    }
+
+    // Using stack 
+    // TC: O(n)
+    // SC: O(n)
+    public int minOperationsStack(String[] logs) {
+        ArrayDeque<String> stack = new ArrayDeque<>();
+        for (String log : logs) {
+            if (log.equals("../")) {
+                if (!stack.isEmpty()) {
+                    stack.pop();
+                }
+            } else if (!log.equals("./")) {
+                stack.push(log);
+            }  
+        }
+        return stack.size();
+    }
+}
