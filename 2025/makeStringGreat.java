2f128c0ec7241b72ad29a7d80142324605be9af9
Suryanandan Babbar <suryanandanbabbar@gmail.com>
2025-07-07 11:42:27 +0530

    1544: Make The String Great

---
+++ 2025/makeStringGreat.java
@@ -0,0 +1,24 @@
+// LeetCode Problem: 1544. Make The String Great
+// Link: https://leetcode.com/problems/make-the-string-great/
+
+import java.util.ArrayDeque;
+
+class makeStringGreat {
+    public String makeGood(String s) {
+        ArrayDeque<Character> stack = new ArrayDeque<>();
+
+        for(char c: s.toCharArray()) {
+            if(!stack.isEmpty() && Math.abs(stack.peek() - c) == 32) {
+                stack.pop();
+            } else {
+                stack.push(c);
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        while(!stack.isEmpty()) {
+            sb.append(stack.pop());
+        }
+        return sb.reverse().toString();
+    }
+}
