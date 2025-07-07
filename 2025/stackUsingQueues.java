fe8f48acaf840e06eca5f270a0432a2e486a0a6a
Suryanandan Babbar <suryanandanbabbar@gmail.com>
2025-07-07 09:34:26 +0530

    225: Implement Stack using Queues

---
+++ 2025/stackUsingQueues.java
@@ -0,0 +1,54 @@
+// LeetCode Problem: 225. Implement Stack using Queues
+// Link: https://leetcode.com/problems/implement-stack-using-queues/
+
+import java.util.LinkedList;
+import java.util.Queue;
+public class stackUsingQueues {
+    // Queues: 
+    private Queue<Integer> queue1;
+    private Queue<Integer> queue2;
+
+    public stackUsingQueues() {
+
+    }
+
+    public void push(int x) {
+        if (queue1 == null) {
+            queue1 = new LinkedList<>();
+        }
+        queue1.offer(x);
+    }
+
+    public int pop() {
+        if (queue1 == null || queue1.isEmpty()) {
+            throw new IllegalStateException("Stack is empty");
+        }
+        while (queue1.size() > 1) {
+            queue2.offer(queue1.poll());
+        }
+        int poppedValue = queue1.poll();
+        Queue<Integer> temp = queue1;
+        queue1 = queue2;
+        queue2 = temp;
+        return poppedValue;
+    }
+
+    public int top() {
+        if (queue1 == null || queue1.isEmpty()) {
+            throw new IllegalStateException("Stack is empty");
+        }
+        while (queue1.size() > 1) {
+            queue2.offer(queue1.poll());
+        }
+        int topValue = queue1.peek();
+        queue2.offer(queue1.poll());
+        Queue<Integer> temp = queue1;
+        queue1 = queue2;
+        queue2 = temp;
+        return topValue;
+    }
+    public boolean empty() {
+        return queue1 == null || queue1.isEmpty();
+    }
+
+}
