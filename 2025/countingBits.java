ced040c33b08d0792b972d525675acccd4c6b7bf
Suryanandan Babbar <suryanandanbabbar@gmail.com>
2025-07-09 11:05:26 +0530

    338: Counting Bits

---
+++ 2025/countingBits.java
@@ -0,0 +1,20 @@
+// LeetCode Problem: 338. Counting Bits
+// Link: https://leetcode.com/problems/counting-bits/
+
+// TC: O(n)
+// SC: O(n) 
+
+public class countingBits {
+    public int[] countBits(int n) {
+        int[] ans = new int[n + 1];
+
+        for(int i = 0; i < n + 1; i++) {
+            // Even Number -> count(n) : count(n / 2)
+            if(i % 2 == 0) ans[i] = ans[i / 2];
+            // Odd Number -> count(n) : count(n / 2) + 1
+            else ans[i] = ans[i / 2] + 1;
+        }
+
+        return ans;
+    }
+}
