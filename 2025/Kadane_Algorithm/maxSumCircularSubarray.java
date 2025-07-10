b62b6872067b8378c9fa383e3ea99ad8d3e0b023
Suryanandan Babbar <suryanandanbabbar@gmail.com>
2025-07-10 11:35:30 +0530

    918: Maximum Sum Circular Subarray

--- 2025/Kadane_Algorithm/maxSumCircularSubarray.java
+++ 2025/Kadane_Algorithm/maxSumCircularSubarray.java
@@ -1,5 +1,6 @@
 package Kadane_Algorithm;
 // LeetCode Problem: 918. Maximum Sum Circular Subarray
+
 // Link: https://leetcode.com/problems/maximum-sum-circular-subarray/
 
 // TC: O(n)
@@ -7,18 +8,18 @@ package Kadane_Algorithm;
 
 public class maxSumCircularSubarray {
     public int maxSubarraySumCir(int[] nums) {
-      int total=0;
-      int currmin=0;
-      int currmax=0;
-      int max=nums[0];
-      int min=nums[0];
-      for(int num:nums){
-          total+=num;
-          currmax=Math.max(currmax+num,num);
-          currmin=Math.min(currmin+num,num);
-          max=Math.max(currmax,max);
-          min=Math.min(currmin,min);
-      }
-      return max<0?max:Math.max(max,total-min);
+        int total = 0;
+        int currmin = 0;
+        int currmax = 0;
+        int max = nums[0];
+        int min = nums[0];
+        for (int num : nums) {
+            total += num;
+            currmax = Math.max(currmax + num, num);
+            currmin = Math.min(currmin + num, num);
+            max = Math.max(currmax, max);
+            min = Math.min(currmin, min);
+        }
+        return max < 0 ? max : Math.max(max, total - min);
     }
 }
