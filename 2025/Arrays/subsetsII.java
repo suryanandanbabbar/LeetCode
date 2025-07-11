98f51b810860d1d91a979fcbc2296b7295979b10
Suryanandan Babbar <suryanandanbabbar@gmail.com>
2025-07-11 18:47:40 +0530

    90: Subsets II

---
+++ 2025/Arrays/subsetsII.java
@@ -0,0 +1,28 @@
+package Arrays;
+// LeetCode Problem: 90. Subsets II
+// Link: https://leetcode.com/problems/subsets-ii/
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class subsetsII {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> outer = new ArrayList<>();
+        outer.add(new ArrayList<>());
+
+        for (int num : nums) {
+            int n = outer.size();
+            for (int i = 0; i < n; i++) {
+                List<Integer> internal = new ArrayList<>(outer.get(i));
+                internal.add(num);
+                if (!outer.contains(internal)) {
+                    outer.add(internal);
+                }
+            }
+        }
+
+        return outer;
+    }
+}
