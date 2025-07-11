81eb51192802c7634003414a180eeecbda792324
Suryanandan Babbar <suryanandanbabbar@gmail.com>
2025-07-11 18:46:16 +0530

    39: Combination Sum

---
+++ 2025/Arrays/combinationSum.java
@@ -0,0 +1,32 @@
+package Arrays;
+// LeetCode Problem: 39. Combination Sum
+// Link: https://leetcode.com/problems/combination-sum/
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class combinationSum {
+    public List<List<Integer>> combination(int[] candidates, int target) {
+        List<List<Integer>> res = new ArrayList<>();
+
+        makeCombination(candidates, target, 0, new ArrayList<>(), 0, res);
+        return res;
+    }
+
+    private void makeCombination(int[] candidates, int target, int idx, List<Integer> comb, int total,
+            List<List<Integer>> res) {
+        if (total == target) {
+            res.add(new ArrayList<>(comb));
+            return;
+        }
+
+        if (total > target || idx >= candidates.length) {
+            return;
+        }
+
+        comb.add(candidates[idx]);
+        makeCombination(candidates, target, idx, comb, total + candidates[idx], res);
+        comb.remove(comb.size() - 1);
+        makeCombination(candidates, target, idx + 1, comb, total, res);
+    }
+}
