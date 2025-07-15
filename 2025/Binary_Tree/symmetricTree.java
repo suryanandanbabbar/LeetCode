8d89152daa9bc0b2072d1435ca33af4d1249233b
Suryanandan Babbar <suryanandanbabbar@gmail.com>
2025-07-15 09:38:54 +0530

    101: Symmetric Tree

---
+++ 2025/Binary_Tree/symmetricTree.java
@@ -0,0 +1,26 @@
+package Binary_Tree;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class symmetricTree {
+    private boolean check(TreeNode p, TreeNode q) {
+        if(p == null && q == null) return true;
+        if(p == null || q == null) return false;
+        return p.val == q.val && check(p.right, q.left) && check(p.left, q.right);
+    }
+    public boolean isSymmetric(TreeNode root) {
+        return check(root.left, root.right);
+    }
+}
+
