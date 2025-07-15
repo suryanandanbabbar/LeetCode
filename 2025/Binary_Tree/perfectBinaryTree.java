8dc5b3b1823dc937788ae5ed8c130817b93d530f
Suryanandan Babbar <suryanandanbabbar@gmail.com>
2025-07-15 15:22:59 +0530

    GfG: Check if Perfect Binary Tree

---
+++ 2025/Binary_Tree/perfectBinaryTree.java
@@ -0,0 +1,34 @@
+package Binary_Tree;
+// GfG: https://www.geeksforgeeks.org/problems/perfect-binary-tree/1
+
+// TC: O(N)
+// SC: O(h)
+
+public class perfectBinaryTree {
+    public boolean isPerfect(Node root) {
+        if (root == null) return true;
+
+        int depth = getDepth(root);
+        return isPerfectUtil(root, depth, 0);
+
+        }
+
+        private int getDepth(Node node) {
+            int d = 0;
+            while (node != null) {
+                d++;
+                node = node.left;
+            }
+            return d;
+        }
+
+        private boolean isPerfectUtil(Node node, int depth, int level) {
+            if (node == null) return true;
+            if (node.left == null && node.right == null)
+                return depth == level + 1;
+            if (node.left == null || node.right == null)
+                return false;
+            return isPerfectUtil(node.left, depth, level + 1) &&
+                   isPerfectUtil(node.right, depth, level + 1);
+    }
+}
