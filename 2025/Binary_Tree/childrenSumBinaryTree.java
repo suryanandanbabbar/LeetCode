b517407050ef28d346d4868b3a638b036f05acdd
Suryanandan Babbar <suryanandanbabbar@gmail.com>
2025-07-16 10:11:53 +0530

    GfG: Children Sum in a Binary Tree

---
+++ 2025/Binary_Tree/childrenSumBinaryTree.java
@@ -0,0 +1,19 @@
+package Binary_Tree;
+// GfG: https://www.geeksforgeeks.org/problems/children-sum-parent/1
+
+// TC: O(N)
+// SC: O(H)
+
+public class childrenSumBinaryTree {
+    public static int isSumProperty(Node root) {
+        if(root == null) return 1;
+        if(root.left == null && root.right == null) return 1;
+        
+        int left = (root.left != null) ? root.left.data : 0;
+        int right = (root.right != null) ? root.right.data : 0;
+        
+        if(root.data != left + right) return 0;
+        
+        return isSumProperty(root.left) & isSumProperty(root.right);
+    }
+}
