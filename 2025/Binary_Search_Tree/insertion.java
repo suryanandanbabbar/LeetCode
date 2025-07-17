51e47559ab410313431bc9bf4e3cc2d3b814358a
Suryanandan Babbar <suryanandanbabbar@gmail.com>
2025-07-17 14:17:54 +0530

    701: Insert into Binary Search Tree

---
+++ 2025/Binary_Search_Tree/insertion.java
@@ -0,0 +1,28 @@
+package Binary_Search_Tree;
+// LeetCode Problem: 701. Insert into Binary Search Tree
+// Link: https://leetcode.com/problems/insert-into-a-binary-search-tree/
+
+// TC: O(n) worst-case
+// For average case, O(log n)
+// SC: O(n) worst-case
+// For average case, O(log n)
+
+public class insertion {
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        if(root == null) return new TreeNode(val);
+        if(val < root.val) {
+            if(root.left == null) {
+                root.left = new TreeNode(val);
+            } else {
+                insertIntoBST(root.left, val);
+            }
+        } else {
+            if(root.right == null) {
+                root.right = new TreeNode(val);
+            } else {
+                insertIntoBST(root.right, val);
+            }
+        }
+        return root;
+    }
+}
