91df427e6c893cf1a5a390ad769cb44a3149bb1a
Suryanandan Babbar <suryanandanbabbar@gmail.com>
2025-07-17 10:31:28 +0530

    GfG: Ceil in a BST

---
+++ 2025/Binary_Search_Tree/ceilBST.java
@@ -0,0 +1,24 @@
+package Binary_Search_Tree;
+// GfG: https://www.geeksforgeeks.org/problems/implementing-ceil-in-bst/1
+
+// TC: O(h)
+// SC: O(1)
+
+public class ceilBST {
+    int findCeil(Node root, int key) {
+        int ceil = -1;
+
+        while (root != null) {
+            if (root.data == key) {
+                return key;
+            } else if (root.data < key) {
+                root = root.right;
+            } else {
+                ceil = root.data;
+                root = root.left;
+            }
+        }
+
+        return ceil;
+    }
+}
