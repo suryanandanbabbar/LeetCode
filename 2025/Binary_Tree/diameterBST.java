368714472315ba93fd76f7621a23b6cffa7942c3
Suryanandan Babbar <suryanandanbabbar@gmail.com>
2025-07-17 14:12:27 +0530

    543: Diameter of Binary Tree

---
+++ 2025/Binary_Tree/diameterBST.java
@@ -0,0 +1,24 @@
+package Binary_Tree;
+// LeetCode Problem: 543. Diameter of Binary Tree
+// Link: https://leetcode.com/problems/diameter-of-binary-tree/
+
+public class diameterBST {
+    int diameter;
+
+    public int solve(TreeNode root) {
+        if(root == null) return 0;
+
+        int left = solve(root.left);
+        int right = solve(root.right);
+        diameter = Math.max(diameter, left + right);
+
+        return Math.max(left, right) + 1;
+    }
+
+    public int diameterOfBinaryTree(TreeNode root) {
+        diameter = 0;
+        solve(root);
+        return diameter;
+    }
+}
+
