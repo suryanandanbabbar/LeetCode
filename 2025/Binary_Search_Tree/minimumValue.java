724f6068cc8b5c87327d32c7286d3b351e65cb00
Suryanandan Babbar <suryanandanbabbar@gmail.com>
2025-07-17 09:51:31 +0530

    GfG: Minimum element in BST

---
+++ 2025/Binary_Search_Tree/minimumValue.java
@@ -0,0 +1,15 @@
+package Binary_Search_Tree;
+// GfG: https://www.geeksforgeeks.org/problems/minimum-element-in-bst/1
+
+// TC: O(H)
+// SC: O(1)
+
+public class minimumValue {
+    int minValue(Node root) {
+        Node current = root;
+        while(current != null && current.left != null) {
+            current = current.left;
+        }
+        return current.data;
+    }
+}
