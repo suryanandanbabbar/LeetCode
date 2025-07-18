0b6540507fafc549f6c7d9ab7a30eb675d6e9aee
Suryanandan Babbar <suryanandanbabbar@gmail.com>
2025-07-18 11:19:01 +0530

    109. Convert Sorted List to Binary Search Tree

---
+++ 2025/Binary_Search_Tree/linkedListToBST.java
@@ -0,0 +1,30 @@
+package Binary_Search_Tree;
+// LeetCode Problem: 109. Convert Sorted List to Binary Search Tree
+// Link: https://leetcode.com/problems/convert-sorted-list-to-binary-search-tree/
+
+// TC: O(N)
+// SC: O(log N) for the recursion stack
+
+public class linkedListToBST {
+    public TreeNode sortedListToBST(ListNode head) {
+        if (head == null)
+            return null;
+        return toBST(head, null);
+    }
+
+    public TreeNode toBST(ListNode head, ListNode tail) {
+        ListNode slow = head;
+        ListNode fast = head;
+        if (head == tail)
+            return null;
+
+        while (fast != tail && fast.next != tail) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        TreeNode thead = new TreeNode(slow.val);
+        thead.left = toBST(head, slow);
+        thead.right = toBST(slow.next, tail);
+        return thead;
+    }
+}
