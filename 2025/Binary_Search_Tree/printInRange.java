1085df1bc2ff90bc73d65a600661381af8cac4a9
Suryanandan Babbar <suryanandanbabbar@gmail.com>
2025-07-18 10:49:27 +0530

    Added Level-Order Traversal Approach

--- 2025/Binary_Search_Tree/printInRange.java
+++ 2025/Binary_Search_Tree/printInRange.java
@@ -11,6 +11,33 @@ public class printInRange {
         ArrayList<Integer> list = new ArrayList<>();
         inOrderRange(root, low, high, list);
         return list;
+
+        // Level-Order Traversal Approach
+        /*
+        ArrayList<Integer> list = new ArrayList<>();
+        Queue<Node> q = new LinkedList<>();
+        
+        if(root == null) {
+            list.add(null);
+            return list;
+        }
+        
+        q.offer(root);
+        while(!q.isEmpty()) {
+            int size = q.size();
+            
+            for(int i = 0; i < size; i++) {
+                Node node = q.poll();
+                
+                if(node.data >= low && node.data <= high) 
+                    list.add(node.data);
+                if(node.left != null) q.offer(node.left);
+                if(node.right != null) q.offer(node.right);
+            }
+        }
+        Collections.sort(list);
+        return list;
+         */
     }
 
     private static void inOrderRange(Node root, int low, int high, ArrayList<Integer> list) {
