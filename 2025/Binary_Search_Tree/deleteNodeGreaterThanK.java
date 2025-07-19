e7d4aa0f79d021e78f44310c9bf908b832efe1c7
Suryanandan Babbar <suryanandanbabbar@gmail.com>
2025-07-19 10:54:32 +0530

    GfG: Delete Nodes Greater than K

---
+++ 2025/Binary_Search_Tree/deleteNodeGreaterThanK.java
@@ -0,0 +1,17 @@
+package Binary_Search_Tree;
+// GfG: https://www.geeksforgeeks.org/problems/delete-nodes-greater-than-k/1
+
+// TC: O(N)
+// SC: O(N) for the recursion stack
+
+public class deleteNodeGreaterThanK {
+    public Node deleteNode(Node root, int k) {
+        if(root == null) return null;    
+        
+        if(root.data < k) {
+            root.right = deleteNode(root.right, k);
+            return root;
+        }
+        else return deleteNode(root.left, k);
+    }
+}
