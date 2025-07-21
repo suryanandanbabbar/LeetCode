6c1748701401917064571dbaead742cc7a0ee64a
Suryanandan Babbar <suryanandanbabbar@gmail.com>
2025-07-21 10:28:13 +0530

    Sum of elements between k1'th and k2'th smallest elements

---
+++ 2025/Heaps/sumK1K2Smallest.java
@@ -0,0 +1,27 @@
+package Heaps;
+// GfG: https://www.geeksforgeeks.org/problems/sum-of-elements-between-k1th-and-k2th-smallest-elements3133/1
+
+// TC: O(N long N)
+// SC: O(N) for the priority queue
+
+import java.util.PriorityQueue;
+
+public class sumK1K2Smallest {
+    public static long sumBetweenTwoKth(long A[], long N, long K1, long K2) {
+        if (K1 > K2 || K1 < 1 || K2 > N) return 0;
+
+        PriorityQueue<Long> minHeap = new PriorityQueue<>();
+        for (long num : A) {
+            minHeap.add(num);
+        }
+
+        long sum = 0;
+        for (long i = 1; i <= K2; i++) {
+            long current = minHeap.poll();
+            if (i > K1 && i < K2) {
+                sum += current;
+            }
+        }
+        return sum;
+    }
+}
