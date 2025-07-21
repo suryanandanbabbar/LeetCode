5cae88ab37948e1a4a5647de48feb231aac5c75e
Suryanandan Babbar <suryanandanbabbar@gmail.com>
2025-07-21 09:48:36 +0530

    GfG: Does array represent Heaps?

---
+++ 2025/Heaps/arrayHeap.java
@@ -0,0 +1,21 @@
+package Heaps;
+// GfG: https://www.geeksforgeeks.org/problems/does-array-represent-heap4345/1
+
+// TC: O(N)
+// SC: O(1)
+
+public class arrayHeap {
+    public boolean countSub(long arr[], long n) {
+        if(n == 0 || n == 1) return true;
+
+        for(long i = 0; i < n / 2; i++) {
+            long left = 2 * i + 1;
+            long right = 2 * i + 2;
+
+            if(left < n && arr[(int)i] < arr[(int)left]) return false;
+            if(right < n && arr[(int)i] < arr[(int)right]) return false;
+        }
+
+        return true;        
+    }
+}
