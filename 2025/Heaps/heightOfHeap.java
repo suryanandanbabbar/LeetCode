109fa663a6933dbac47f041647923d31035987ae
Suryanandan Babbar <suryanandanbabbar@gmail.com>
2025-07-21 10:48:04 +0530

    GfG: Height of Heap

---
+++ 2025/Heaps/heightOfHeap.java
@@ -0,0 +1,18 @@
+package Heaps;
+// GfG: https://www.geeksforgeeks.org/problems/height-of-heap5025/1
+
+// TC: O(log N)
+// SC: O(1)
+
+public class heightOfHeap {
+    static int heapHeight(int n, int arr[]) {
+        if (n == 1) return 1;
+
+        int height = 0;
+        while (n > 1) {
+            height++;
+            n /= 2; 
+        }
+        return height; 
+    }
+}
