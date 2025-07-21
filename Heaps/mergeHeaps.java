044f85e2c43047fc4228d96cf0414ddec43cb0ff
Suryanandan Babbar <suryanandanbabbar@gmail.com>
2025-07-21 15:32:59 +0530

    GfG: Merge two heaps

---
+++ Heaps/mergeHeaps.java
@@ -0,0 +1,26 @@
+package Heaps;
+// GfG: https://www.geeksforgeeks.org/problems/merge-two-binary-max-heap0144/1
+
+// TC: O((N + M) log(N + M))
+// SC: O(N + M) for the priority queue
+
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class mergeHeaps {
+    public int[] merge(int[] a, int[] b, int n, int m) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        
+        for(int i : a) pq.add(i);
+        for(int i : b) pq.add(i);
+        
+        
+        int size = pq.size();
+        int[] result = new int[size];
+        for(int i = 0; i < size; i++)
+            result[i] = pq.poll();
+            
+        return result;
+    }
+    
+}
