dd29e50132732ba10f827a7b1a2f3dd370a181f1
Suryanandan Babbar <suryanandanbabbar@gmail.com>
2025-07-21 10:51:02 +0530

    GfG: Merge K Sorted Arrays

---
+++ 2025/Heaps/mergeKSortedArrays.java
@@ -0,0 +1,24 @@
+package Heaps;
+// GfG: https://www.geeksforgeeks.org/problems/merge-k-sorted-arrays/1
+
+// TC: O(N log K)
+// SC: O(N) for the priority queue
+
+import java.util.*;
+
+public class mergeKSortedArrays {
+    public static ArrayList<Integer> mergeKArrays(int[][] arr, int K) {
+        PriorityQueue<Integer> p = new PriorityQueue<>();
+        ArrayList<Integer> result = new ArrayList<>();
+
+        for (int i = 0; i < K; i++) {
+            for (int j = 0; j < arr[i].length; j++)
+                p.add(arr[i][j]);
+        }
+
+        while (!p.isEmpty())
+            result.add(p.poll());
+
+        return result;
+    }
+}
