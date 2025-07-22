2ac48652c3fe9be2d1d48d197568390fe2d7e50d
Suryanandan Babbar <suryanandanbabbar@gmail.com>
2025-07-22 09:33:06 +0530

    2558. Take Gifts From the Richest Pile

---
+++ Heaps/giftsRichestPile.java
@@ -0,0 +1,25 @@
+package Heaps;
+// LeetCode Problem: 2558. Take Gifts From the Richest Pile
+// Link: https://leetcode.com/problems/take-gifts-from-the-richest-pile/
+
+// TC: O(k log N) where N is the number of gifts
+// SC: O(N) for the priority queue
+
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class giftsRichestPile {
+    public long pickGifts(int[] gifts, int k) {
+        // Max heap
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        long sum = 0;
+        
+        for(int i: gifts) pq.offer(i);
+
+        while(k-- > 0) pq.add((int)Math.sqrt(pq.poll()));
+
+        while(!pq.isEmpty()) sum += pq.poll();
+
+        return sum;
+    } 
+}
