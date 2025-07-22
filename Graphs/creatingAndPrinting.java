9018352db4a556fb63cb4036f88b7e49e1426f16
Suryanandan Babbar <suryanandanbabbar@gmail.com>
2025-07-22 11:29:14 +0530

    GfG: Creating and Printing

---
+++ Graphs/creatingAndPrinting.java
@@ -0,0 +1,39 @@
+package Graphs;
+// GfG: https://www.naukri.com/code360/problems/creating-and-printing_1214551
+
+import java.util.ArrayList;
+
+// TC: O(V + E) where V is the number of vertices and E is the number of edges
+// SC: O(V + E) for the adjacency list
+
+public class creatingAndPrinting {
+    public static int[][] printAdjacency(int n, int m, int[][] edges) {
+        @SuppressWarnings("unchecked")
+        ArrayList<Integer>[] list = (ArrayList<Integer>[]) new ArrayList[n];
+ 
+        for(int i = 0; i < n; i++)
+            list[i] = new ArrayList<>();
+        
+        for(int i = 0; i < m; i++) {
+            int u = edges[i][0];
+            int v = edges[i][1];
+            
+            list[u].add(v);
+            list[v].add(u);
+        }
+        
+        
+        int[][] result = new int[n][];
+        for(int i = 0; i < n; i++){
+            list[i].add(0,i);
+            int k = list[i].size();
+            
+            result[i] = new int[k];
+            
+            for(int j = 0;j < k; j++) 
+                result[i][j] = list[i].get(j);
+        }
+ 
+        return result;
+    }
+}
