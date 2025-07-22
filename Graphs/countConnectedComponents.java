45388ee1a43493c8c7632a69ace3e7813d7a9586
Suryanandan Babbar <suryanandanbabbar@gmail.com>
2025-07-22 11:36:48 +0530

    NeetCode: Number of Connected Components in an Undirected Graph

---
+++ Graphs/countConnectedComponents.java
@@ -0,0 +1,45 @@
+package Graphs;
+// NeetCode: https://neetcode.io/problems/count-connected-components
+
+// TC: O(V + E) where V is the number of vertices and E is the number of edges
+// SC: O(V + E) for the adjacency list and visited array
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class countConnectedComponents {
+    public int countComponents(int n, int[][] edges) {
+        List<List<Integer>> adj = new ArrayList<>(n);
+        boolean[] visited = new boolean[n];
+        int count = 0;
+
+        for (int i = 0; i < n; i++)
+            adj.add(new ArrayList<>());
+
+        for (int[] edge : edges) {
+            int u = edge[0];
+            int v = edge[1];
+            
+            adj.get(u).add(v);
+            adj.get(v).add(u);
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                dfs(i, adj, visited);
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    private void dfs(int node, List<List<Integer>> adj, boolean[] visited) {
+        visited[node] = true;
+        for (int neighbor : adj.get(node)) {
+            if (!visited[neighbor]) {
+                dfs(neighbor, adj, visited);
+            }
+        }
+    }
+}
