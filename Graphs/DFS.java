5afd35f5eff05e98bdec317e65897540692bec6d
Suryanandan Babbar <suryanandanbabbar@gmail.com>
2025-07-22 10:54:25 +0530

    GfG: DFS traversal of Graph

---
+++ Graphs/DFS.java
@@ -0,0 +1,33 @@
+package Graphs;
+// GfG: https://www.geeksforgeeks.org/problems/depth-first-traversal-for-a-graph/1
+
+// TC: O(V + E) where V is the number of vertices and E is the number of edges
+// SC: O(V) for the visited array and result list
+
+import java.util.ArrayList;
+
+public class DFS {
+    public ArrayList<Integer> dfs(ArrayList<ArrayList<Integer>> adj) {
+        ArrayList<Integer> result = new ArrayList<>();
+        boolean[] visited = new boolean[adj.size()];
+
+        for (int i = 0; i < adj.size(); i++) {
+            if (!visited[i]) {
+                dfs(i, adj, visited, result);
+            }
+        }
+
+        return result;
+    }
+
+    public void dfs(int node, ArrayList<ArrayList<Integer>> adj, boolean[] visited, ArrayList<Integer> result) {
+        visited[node] = true;
+        result.add(node);
+
+        for (int neighbor : adj.get(node)) {
+            if (!visited[neighbor]) {
+                dfs(neighbor, adj, visited, result);
+            }
+        }
+    }
+}
