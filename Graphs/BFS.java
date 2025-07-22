d92c52a338d17a99c3a9f82f59e4f91718391adb
Suryanandan Babbar <suryanandanbabbar@gmail.com>
2025-07-22 10:26:31 +0530

    GfG: BFS traversal of Graph

---
+++ Graphs/BFS.java
@@ -0,0 +1,34 @@
+package Graphs;
+// GfG: https://www.geeksforgeeks.org/problems/bfs-traversal-of-graph/1
+
+// TC: O(V + E) where V is the number of vertices and E is the number of edges
+// SC: O(V + E) for the visited array and queue
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class BFS {
+    public ArrayList<Integer> bfs(ArrayList<ArrayList<Integer>> adj) {
+        ArrayList<Integer> result = new ArrayList<>();
+        Queue<Integer> q = new LinkedList<>();
+        int[] visited = new int[adj.size() + 1];
+        
+        q.add(0);
+        visited[0] = 1;
+        
+        while(!q.isEmpty()) {
+            int node = q.poll();
+            result.add(node);
+            
+            for(int i : adj.get(node)) {
+                if(visited[i] != 1) {
+                    visited[i] = 1;
+                    q.offer(i);
+                }
+            }
+        }
+        return result;
+        
+    }
+}
