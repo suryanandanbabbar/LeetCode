6fa80a30bd405f8a8c36c2b0f3cc9625efcbfd00
Suryanandan Babbar <suryanandanbabbar@gmail.com>
2025-07-23 14:34:02 +0530

    542. 01 Matrix

---
+++ Graphs/Matrix01.java
@@ -0,0 +1,56 @@
+package Graphs;
+// LeetCode Problem: 542. 01 Matrix
+// Link: https://leetcode.com/problems/01-matrix/description/
+
+// TC: O(m * n)
+// SC: O(m * n)
+
+import java.util.Queue;
+import java.util.LinkedList;
+
+public class Matrix01 {
+    public int[][] updateMatrix(int[][] mat) {
+        int[][] result = new int[mat.length][mat[0].length];
+        int rows = mat.length;
+        int cols = mat[0].length;
+
+        // Using BFS
+        Queue<int[]> q = new LinkedList<>();
+        boolean[][] visited = new boolean[rows][cols];
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (mat[i][j] == 0) {
+                    q.offer(new int[] { i, j });
+                    visited[i][j] = true;
+                }
+            }
+        }
+
+        int[][] dirs = { { 1, 0 }, { -1, 0 }, { 0, 1 }, { 0, -1 } };
+        int distance = 0;
+
+        while (!q.isEmpty()) {
+            int size = q.size();
+            distance++;
+
+            while (size-- > 0) {
+                int[] cell = q.poll();
+                int x = cell[0];
+                int y = cell[1];
+
+                for (int[] dir : dirs) {
+                    int i = x + dir[0];
+                    int j = y + dir[1];
+
+                    if (i >= 0 && i < rows && j >= 0 && j < cols && !visited[i][j]) {
+                        visited[i][j] = true;
+                        result[i][j] = distance;
+                        q.offer(new int[] { i, j });
+                    }
+                }
+            }
+        }
+        return result;
+    }
+}
