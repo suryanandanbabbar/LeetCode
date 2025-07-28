61dac3c7362b6d14259fb9d800b8eeb9189395af
Suryanandan Babbar <suryanandanbabbar@gmail.com>
2025-07-28 14:47:56 +0530

    Updated code

--- Dynamic_Programming/uniquePaths.java
+++ Dynamic_Programming/uniquePaths.java
@@ -8,21 +8,14 @@ package Dynamic_Programming;
 class uniquePaths {
     public int unique(int m, int n) {
         int[][] dp = new int[m + 1][n + 1];
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                dp[i][j] = -1;
-            }
-        }
+        for(int i = 0; i < m; i++) for(int j = 0; j < n; j++) dp[i][j] = -1;
         return solve(0, 0, m, n, dp);
     }
 
     private int solve(int i, int j, int m, int n, int[][] dp) {
-        if (i == m - 1 && j == n - 1)
-            return 1;
-        if (i >= m || j >= n)
-            return 0;
-        if (dp[i][j] != -1)
-            return dp[i][j];
+        if(i == m - 1 && j == n - 1) return 1;
+        if(i >= m || j >= n) return 0;
+        if(dp[i][j] != -1) return dp[i][j];
 
         return dp[i][j] = solve(i + 1, j, m, n, dp) + solve(i, j + 1, m, n, dp);
     }
