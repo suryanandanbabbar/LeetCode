3c43ff815b8151a42519c0f23eb7d5518c124517
Suryanandan Babbar <suryanandanbabbar@gmail.com>
2025-07-28 09:39:11 +0530

    Added Tabulation Approach

--- Dynamic_Programming/fibonacciSeries.java
+++ Dynamic_Programming/fibonacciSeries.java
@@ -6,10 +6,22 @@ package Dynamic_Programming;
 // SC: O(n)
 
 class fibonacciSeries {
+    // Memoization approach
     public int fib(int n) {
         int[] dp = new int[n + 1];
         if(n == 0 || n == 1) return n;
         if(dp[n] == -1) return dp[n];
         return dp[n] = fib(n - 1) + fib(n - 2);
+    }
+
+    // Tabulation approach
+    public int fibTab(int n) {
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
     }
 }
