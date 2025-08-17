061edbfa703add3c1cf663552ded0bc2c61912ba
Suryanandan Babbar <suryanandanbabbar@gmail.com>
2025-08-17 17:32:47 +0530

    837. New 21 Game

---
+++ Dynamic_Programming/new21Game.java
@@ -0,0 +1,28 @@
+package Dynamic_Programming;
+// LeetCode 837. New 21 Game
+// https://leetcode.com/problems/new-21-game/description/
+
+// TC: O(n)
+// SC: O(n)
+
+public class new21Game {
+    public double newGame(int n, int k, int maxPts) {
+        if (k == 0 || n >= k + maxPts) return 1.0;
+
+        double[] dp = new double[n + 1];
+        dp[0] = 1.0;
+
+        double sum = 1.0;
+        double result = 0.0;
+
+        for (int i = 1; i <= n; i++) {
+            dp[i] = sum / maxPts;
+
+            if (i < k) sum += dp[i];
+            else result += dp[i];
+        
+            if (i - maxPts >= 0) sum -= dp[i - maxPts]; 
+        }
+        return result;
+    }
+}
