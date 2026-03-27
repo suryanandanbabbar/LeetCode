fe5c4b39ada034ac2f8814188a51d14339c04b95
Suryanandan Babbar <140876588+suryanandanbabbar@users.noreply.github.com>
2026-03-27 17:56:09 +0530

    2946-matrix-similarity-after-cyclic-shifts

---
+++ 2946-matrix-similarity-after-cyclic-shifts/2946-matrix-similarity-after-cyclic-shifts.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean areSimilar(int[][] mat, int k) {
+        int m = mat.length;
+        int n = mat[0].length;
+        int shift = k % n;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i % 2 == 0) {
+                    if (mat[i][j] != mat[i][(j + shift) % n]) return false;
+                } else {
+                    if (mat[i][j] != mat[i][(j + n - shift) % n]) return false;
+                }
+            }
+        }
+        return true;
+    }
+}
