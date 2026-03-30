a8859b2dd6932b039ebba320d78d854aa14cfc1d
Suryanandan Babbar <140876588+suryanandanbabbar@users.noreply.github.com>
2026-03-30 17:00:56 +0530

    0242-valid-anagram

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,20 +1,22 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if(s.length() != t.length()) return false;
+        if (s.length() != t.length())
+            return false;
 
         HashMap<Character, Integer> map1 = new HashMap<>();
         HashMap<Character, Integer> map2 = new HashMap<>();
 
-        for(int i = 0; i < s.length(); i++) {
+        for (int i = 0; i < s.length(); i++) {
             map1.put(s.charAt(i), map1.getOrDefault(s.charAt(i), 0) + 1);
         }
 
-        for(int i = 0; i < t.length(); i++) {
+        for (int i = 0; i < t.length(); i++) {
             map2.put(t.charAt(i), map2.getOrDefault(t.charAt(i), 0) + 1);
         }
 
-        for(char c: map1.keySet()) {
-            if(map1.get(c) != map2.get(c)) return false;
+        for (char c : map1.keySet()) {
+            if (!map1.get(c).equals(map2.get(c)))
+                return false;
         }
 
         return true;
