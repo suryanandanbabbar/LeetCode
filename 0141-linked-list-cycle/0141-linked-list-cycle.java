fb6507105e845c8d3b8411a12b4efae3a783e549
Suryanandan Babbar <140876588+suryanandanbabbar@users.noreply.github.com>
2026-03-30 15:22:58 +0530

    0141-linked-list-cycle

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -11,16 +11,17 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        HashSet<ListNode> set = new HashSet<>();
-        ListNode temp = head;
+        if(head == null || head.next == null) return false;
+        
+        ListNode slow = head;
+        ListNode fast = head;
 
-        while(temp != null) {
-            if(set.contains(temp)) return true;
-            set.add(temp);
-            temp = temp.next;
+        while(slow != fast) {
+            if(fast == null || fast.next == null) return false;
+            slow = slow.next;
+            fast = fast.next.next;
         }
 
-        return false;
-
+        return true;
     }
 }
