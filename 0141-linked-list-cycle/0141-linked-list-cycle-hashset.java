3a28a7030af4f0a764dd53e3fcbb35925ba85763
Suryanandan Babbar <140876588+suryanandanbabbar@users.noreply.github.com>
2026-03-30 15:19:02 +0530

    0141-linked-list-cycle

---
+++ 0141-linked-list-cycle/0141-linked-list-cycle-hashset.java
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        HashSet<ListNode> set = new HashSet<>();
+        ListNode temp = head;
+
+        while(temp != null) {
+            if(set.contains(temp)) return true;
+            set.add(temp);
+            temp = temp.next;
+        }
+
+        return false;
+
+    }
+}
