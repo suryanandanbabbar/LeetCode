633b41f674f1dda735c68d55085a14421050cfb0
Suryanandan Babbar <140876588+suryanandanbabbar@users.noreply.github.com>
2026-03-30 13:25:59 +0530

    0003-longest-substring-without-repeating-characters

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -5,14 +5,14 @@ class Solution {
         int left = 0;
         int ans = 0;
         for(int right = 0; right < n; right++) {
-            int len = 0;
+
             while(set.contains(s.charAt(right))) {
                 set.remove(s.charAt(left));
                 left++;
             }
             
             set.add(s.charAt(right));
-            ans = Math.max(set.size(), ans);
+            ans = Math.max(ans, right - left + 1);
         }
 
         return ans;
