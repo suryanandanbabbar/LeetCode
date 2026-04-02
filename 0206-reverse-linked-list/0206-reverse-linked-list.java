25c87936b0d982aefcebebd57601323effabe5eb
Suryanandan Babbar <140876588+suryanandanbabbar@users.noreply.github.com>
2026-04-02 14:08:53 +0530

    0206-reverse-linked-list

--- 0206-reverse-linked-list/0206-reverse-linked-list.java
+++ 0206-reverse-linked-list/0206-reverse-linked-list.java
@@ -12,7 +12,7 @@ class Solution {
     public ListNode reverseList(ListNode head) {
         ListNode prev = null;
         ListNode curr = head;
-        
+
         while(curr != null) {
             ListNode temp = curr.next;
             curr.next = prev;
@@ -21,6 +21,6 @@ class Solution {
         }
 
         return prev;
-
+        
     }
 }
