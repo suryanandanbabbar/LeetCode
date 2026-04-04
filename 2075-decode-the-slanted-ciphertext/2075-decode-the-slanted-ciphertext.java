f08f1e33c2cce667a08ce686a1c84ec02b125f5d
Suryanandan Babbar <140876588+suryanandanbabbar@users.noreply.github.com>
2026-04-04 11:52:36 +0530

    2075-decode-the-slanted-ciphertext

---
+++ 2075-decode-the-slanted-ciphertext/2075-decode-the-slanted-ciphertext.java
@@ -0,0 +1,31 @@
+class Solution {
+    public String decodeCiphertext(String encodedText, int rows) {
+        if (rows == 0)
+            return "";
+
+        int n = encodedText.length();
+        if (n == 0)
+            return "";
+
+        int cols = n / rows;
+
+        StringBuilder result = new StringBuilder();
+
+        for (int startCol = 0; startCol < cols; startCol++) {
+            int i = 0, j = startCol;
+
+            while (i < rows && j < cols) {
+                result.append(encodedText.charAt(i * cols + j));
+                i++;
+                j++;
+            }
+        }
+
+        int end = result.length() - 1;
+        while (end >= 0 && result.charAt(end) == ' ') {
+            end--;
+        }
+
+        return result.substring(0, end + 1);
+    }
+}
