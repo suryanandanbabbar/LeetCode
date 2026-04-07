8730b0287700564ef2ca9aa649527c41d2ed960a
Suryanandan Babbar <140876588+suryanandanbabbar@users.noreply.github.com>
2026-04-07 13:36:50 +0530

    0146-lru-cache

--- 0146-lru-cache/0146-lru-cache.java
+++ 0146-lru-cache/0146-lru-cache.java
@@ -1,3 +1,4 @@
+// node
 class Node {
     int key;
     int value;
@@ -14,63 +15,74 @@ class Node {
 
 class LRUCache {
     int capacity;
-    HashMap<Integer, Node> cache; // Map key to nodes
-    Node left; // Least Recently Used
-    Node right; // Most Recently Used
+    // HashMap (key -> nodes)
+    HashMap<Integer, Node> cache;
+    // Left pointer for LRU, right pointer for MRU
+    Node left;
+    Node right;
+
 
     public LRUCache(int capacity) {
-        this.capacity = capacity;    
-        this.cache = new HashMap<>(); 
+        this.capacity = capacity;
+        this.cache = new HashMap<>();
         this.left = new Node(0,0);
         this.right = new Node(0,0);
-        this.left.next = this.right;
-        this.right.prev = this.left;
+        // Link left and right for doubly linked list
+        this.left.next = right;
+        this.right.prev = left;
     }
     
-    // Remove node from the list
-    private void remove(Node node) {
-        Node prev = node.prev;
-        Node next = node.next;
-        prev.next = next;
-        next.prev = prev;
-    }
-    
-    // Insert node into the list at right
-    private void insert(Node node) {
-        Node prev = right.prev;
-        Node next = right;
-        prev.next = next.prev = node;
-        node.next = next;
-        node.prev = prev;
-    }
-
     public int get(int key) {
         if(cache.containsKey(key)) {
             Node node = cache.get(key);
+            // Remove the node from the list
             remove(node);
+            // Insert the node at the rightmost side
             insert(node);
             return node.value;
         }
+
         return -1;
     }
     
     public void put(int key, int value) {
         if(cache.containsKey(key)) {
+            // Update the value of the key : cache.get(key);
+            // Before we can insert the new (key-value) pair, we need to remove it from the list
             remove(cache.get(key));
         }
-        
-        Node newNode = new Node(key, value);
-        cache.put(key, newNode); // Point key to the newNode
-        insert(newNode); // Insert into linked list
 
+        Node newNode = new Node(key, value);
+        // Point the key to the newNode
+        cache.put(key, newNode);
+        // Insert into the linked list at right end
+        insert(newNode);
+        
+        // If capacity is not enough
         if(cache.size() > capacity) {
-            // Remove the list and delete the LRU from the hashmap
+            // Remove the LRU, that is, left 
+            // Remove from the list and from the hashmap
             Node lru = left.next;
             remove(lru);
             cache.remove(lru.key);
-
         }
+    }
 
+    // Helper method for insertion at the right end
+    private void insert(Node node) {
+        Node prev = right.prev;
+        Node next = right;
+
+        prev.next = next.prev = node;
+        node.next = next;
+        node.prev = prev;
+    }
+
+    private void remove(Node node) {
+        Node prev = node.prev;
+        Node next = node.next;
+        prev.next = next;
+        next.prev = prev;
     }
 }
 
