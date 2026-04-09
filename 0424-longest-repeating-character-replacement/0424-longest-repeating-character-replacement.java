0dc11cc7b106e4e64fcb08509e7c9d12505ba024
Suryanandan Babbar <140876588+suryanandanbabbar@users.noreply.github.com>
2026-04-09 13:41:59 +0530

    0424-longest-repeating-character-replacement

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -1,31 +1,35 @@
 class Solution {
     public int characterReplacement(String s, int k) {
+        int left = 0;
+        int right = 0;
+        int ans = 0;
         HashMap<Character, Integer> map = new HashMap<>();
-        int n = s.length();
 
-        int left = 0, right = 0, ans = 0;
+        while(right < s.length()) {
+            // window length
+            int window = right - left + 1;
 
-        while(right < n) {
-            // Length of the window
-            int windowLength = right - left + 1;
-            
-            // Updating hashmap for the frequency
             map.put(s.charAt(right), map.getOrDefault(s.charAt(right), 0) + 1);
 
-            // Maximum Frequency
-            int max = 0;
-            for(int f: map.values()) max = Math.max(max, f);
+            // Max occurence
+            int f = 0;
+            for(int i : map.values()) f = Math.max(f, i);
 
-            if(windowLength - max <= k) {
+            // window length - count[max occurring character] <= k
+            // ans = right - left + 1
+            if(window - f <= k) {
                 ans = Math.max(ans, right - left + 1);
             }
-            if(windowLength - max > k) {
+
+            // window length - count[max occurring character] > k
+            // decrement the frequency of the character from left
+            // shrink the window -> left++;
+            if(window - f > k) {
                 map.put(s.charAt(left), map.get(s.charAt(left)) - 1);
                 left++;
             }
-
             right++;
-        } 
+        }
 
         return ans;
     }
