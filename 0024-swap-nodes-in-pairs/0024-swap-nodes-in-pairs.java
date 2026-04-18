c0419e163c864b9c8fb5f20e84613020229116a0
Suryanandan Babbar <140876588+suryanandanbabbar@users.noreply.github.com>
2026-04-18 18:56:16 +0530

    0024-swap-nodes-in-pairs

---
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+        ListNode dummy = new ListNode(0, head);
+        ListNode prev = dummy;
+        ListNode curr = head;
+
+        while(curr != null && curr.next != null) {
+            ListNode a = curr.next.next;
+            ListNode b = curr.next;
+
+            b.next = curr;
+            curr.next = a;
+            prev.next = b;
+
+            prev = curr;
+            curr = a;
+        }
+
+        return dummy.next;
+    }
+
+}
