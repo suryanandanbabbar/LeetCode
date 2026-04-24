782d7dfd993db061e6dfe21e4cfecc5a9fefb844
Suryanandan Babbar <140876588+suryanandanbabbar@users.noreply.github.com>
2026-04-24 18:38:14 +0530

    2833-furthest-point-from-origin

---
+++ 2833-furthest-point-from-origin/2833-furthest-point-from-origin.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int furthestDistanceFromOrigin(String moves) {
+        int l = 0, r = 0, x = 0;
+
+        for(char c: moves.toCharArray()) {
+            if(c == 'L') l++;
+            else if(c == 'R') r++;
+            else x++;
+        }
+
+        return Math.abs(l - r) + x;
+    }
+}
