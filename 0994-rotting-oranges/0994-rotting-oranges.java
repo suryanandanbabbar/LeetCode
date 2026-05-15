9e44605c36b3b9a6fd890c08461b8f1aa61ee3ab
Suryanandan Babbar <140876588+suryanandanbabbar@users.noreply.github.com>
2026-05-15 16:01:47 +0530

    0994-rotting-oranges

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -1,38 +1,39 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
-        int m = grid.length;
-        int n = grid[0].length;
+        int n = grid.length;
+        int m = grid[0].length;
 
-        for(int i = 0; i < m; i++) {
-            for(int j = 0; j < n; j++) {
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
                 if(grid[i][j] == 2) {
                     dfs(i, j, grid, 2);
                 }
             }
         }
-        int count = 2;
 
+        int count = 2;
         for(int[] r: grid) {
             for(int c: r) {
                 if(c == 1) return - 1;
                 count = Math.max(count, c);
             }
         }
-        
         return count - 2;
     }
 
-    private void dfs(int i, int j, int[][] grid, int count) {
-        if(i < 0 || i >= grid.length 
-        || j < 0 || j >= grid[0].length 
-        || grid[i][j] == 0 
-        || (grid[i][j] > 1 && grid[i][j] < count)) return;
+    private void dfs(int i, int j, int[][] grid, int temp) {
+        if(i < 0 || j < 0
+        || i >= grid.length 
+        || j >= grid[0].length
+        || grid[i][j] == 0
+        || (grid[i][j] > 1 && grid[i][j] < temp))
+            return;
 
-        grid[i][j] = count;
+        grid[i][j] = temp;
 
-        dfs(i + 1, j, grid, count + 1);
-        dfs(i - 1, j, grid, count + 1);
-        dfs(i, j + 1, grid, count + 1);
-        dfs(i, j - 1, grid, count + 1);
+        dfs(i + 1, j, grid, temp + 1);
+        dfs(i - 1, j, grid, temp + 1);
+        dfs(i, j + 1, grid, temp + 1);
+        dfs(i, j - 1, grid, temp + 1);
     }
 }
