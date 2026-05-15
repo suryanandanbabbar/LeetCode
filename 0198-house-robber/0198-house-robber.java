2c627544cda7994900dafbde177953572bc3a112
Suryanandan Babbar <140876588+suryanandanbabbar@users.noreply.github.com>
2026-05-15 16:09:43 +0530

    0198-house-robber

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -2,8 +2,7 @@ class Solution {
     public int rob(int[] nums) {
         int n = nums.length;
         if(n == 1) return nums[0];
-        int[] dp = new int[n];
-
+        int[] dp = new int[n + 1];
         dp[0] = nums[0];
         dp[1] = Math.max(nums[0], nums[1]);
 
