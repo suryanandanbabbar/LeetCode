3e6a951197aa9ca4eb28e9dfb73eefea1c2ef567
Suryanandan Babbar <140876588+suryanandanbabbar@users.noreply.github.com>
2026-05-15 15:47:26 +0530

    0283-move-zeroes

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,13 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int left = 0, right = 0;
+        for(; right < nums.length; right++) {
+            if(nums[right] != 0) {
+                int temp = nums[right];
+                nums[right] = nums[left];
+                nums[left] = temp;
+                left++;
+            }
+        }
+    }
+}
