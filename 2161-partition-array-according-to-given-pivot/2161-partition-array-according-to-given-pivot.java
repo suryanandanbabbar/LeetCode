bb3a564eb10f407c47365435e079a79e221a3551
Suryanandan Babbar <140876588+suryanandanbabbar@users.noreply.github.com>
2026-06-08 12:54:16 +0530

    2161-partition-array-according-to-given-pivot

--- 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.java
+++ 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.java
@@ -1,35 +1,27 @@
 class Solution {
-    public int[] pivotArray(int[] nums, int pivot) {
-
-        List<Integer> smaller = new ArrayList<>();
-        List<Integer> equal = new ArrayList<>();
-        List<Integer> greater = new ArrayList<>();
-
-        for (int num : nums) {
-            if (num < pivot) {
-                smaller.add(num);
-            } else if (num == pivot) {
-                equal.add(num);
-            } else {
-                greater.add(num);
+    static{
+        for(int i=0;i<300;i++) pivotArray(new int[2],0);
+    }
+    public static int[] pivotArray(int[] nums, int pivot) {
+        int n = nums.length;
+        int[] result = new int[n];
+        
+        int left = 0;
+        int right = n - 1;
+        
+        for (int i = 0, j = n - 1; i < n; i++, j--) {
+            if (nums[i] < pivot) {
+                result[left++] = nums[i];
+            }
+            if (nums[j] > pivot) {
+                result[right--] = nums[j];
             }
         }
-
-        int[] result = new int[nums.length];
-        int index = 0;
-
-        for (int num : smaller) {
-            result[index++] = num;
+        
+        while (left <= right) {
+            result[left++] = pivot;
         }
-
-        for (int num : equal) {
-            result[index++] = num;
-        }
-
-        for (int num : greater) {
-            result[index++] = num;
-        }
-
+        
         return result;
     }
 }
