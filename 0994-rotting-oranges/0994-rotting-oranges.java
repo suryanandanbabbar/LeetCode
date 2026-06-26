27b689324ecab3fc290ab6e02ee517f90599f446
Suryanandan Babbar <140876588+suryanandanbabbar@users.noreply.github.com>
2026-06-26 11:21:43 +0530

    0994-rotting-oranges

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -6,34 +6,34 @@ class Solution {
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < m; j++) {
                 if(grid[i][j] == 2) {
-                    dfs(i, j, grid, 2);
-                }
+                    solve(i, j, grid, 2);
+                } 
             }
-        }
+        }   
 
         int count = 2;
         for(int[] r: grid) {
             for(int c: r) {
-                if(c == 1) return - 1;
+                if(c == 1) return -1;
                 count = Math.max(count, c);
             }
         }
+
         return count - 2;
     }
 
-    private void dfs(int i, int j, int[][] grid, int temp) {
-        if(i < 0 || j < 0
-        || i >= grid.length 
-        || j >= grid[0].length
+    private void solve(int i, int j, int[][] grid, int count) {
+        if(i < 0 || i >= grid.length
+        || j < 0 || j >= grid[0].length
         || grid[i][j] == 0
-        || (grid[i][j] > 1 && grid[i][j] < temp))
-            return;
+        || (grid[i][j] > 1 && grid[i][j] < count)
+        ) return;
 
-        grid[i][j] = temp;
+        grid[i][j] = count;
 
-        dfs(i + 1, j, grid, temp + 1);
-        dfs(i - 1, j, grid, temp + 1);
-        dfs(i, j + 1, grid, temp + 1);
-        dfs(i, j - 1, grid, temp + 1);
+        solve(i + 1, j, grid, count + 1);
+        solve(i - 1, j, grid, count + 1);
+        solve(i, j + 1, grid, count + 1);
+        solve(i, j - 1, grid, count + 1);
     }
 }
